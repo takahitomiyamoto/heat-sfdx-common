e332fc633a7e5748f9728e95f07192ad577fc66f
takahito <takahito@MacBook-Pro.local>
2020-07-11 18:29:12 +0900

    [add] :sparkles: fs

---
+++ src/fs.ts
@@ -0,0 +1,40 @@
+/**
+ * @name fs.ts
+ * @description utilities for fs
+ */
+import fs from 'fs';
+
+const UTF8 = 'utf8';
+
+/**
+ * @name readFileSyncUtf8
+ * @description readFileSync with encoding: Utf8
+ */
+const readFileSyncUtf8 = (path: string) => {
+  return fs.readFileSync(path, {
+    encoding: UTF8
+  });
+};
+
+/**
+ * @name readdirSyncUtf8
+ * @description readdirSync with encoding: Utf8
+ */
+const readdirSyncUtf8 = (path: fs.PathLike) => {
+  return fs.readdirSync(path, {
+    encoding: UTF8,
+    withFileTypes: true
+  });
+};
+
+/**
+ * @name writeFileSyncUtf8
+ * @description writeFileSync with encoding: Utf8
+ */
+const writeFileSyncUtf8 = (path: string, data: string) => {
+  fs.writeFileSync(path, data, {
+    encoding: UTF8
+  });
+};
+
+export { readFileSyncUtf8, readdirSyncUtf8, writeFileSyncUtf8 };
--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,15 @@
 import { authorization, loginJwt } from './auth';
 import { httpRequest, requestBody2String } from './https';
 import { json2xml } from './xml';
+import { readFileSyncUtf8, readdirSyncUtf8, writeFileSyncUtf8 } from './fs';
 
-export { authorization, loginJwt, httpRequest, requestBody2String, json2xml };
+export {
+  authorization,
+  loginJwt,
+  httpRequest,
+  requestBody2String,
+  json2xml,
+  readFileSyncUtf8,
+  readdirSyncUtf8,
+  writeFileSyncUtf8
+};
